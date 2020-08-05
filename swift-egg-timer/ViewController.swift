08105e7b2314e297ac99e5863a952a53847ecb7b
AechGG <hazg@me.com>
2020-08-05 15:57:28 +0100

    feat: play sound at end

--- swift-egg-timer/ViewController.swift
+++ swift-egg-timer/ViewController.swift
@@ -7,12 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var progressBar: UIProgressView!
     
+    var player: AVAudioPlayer?
     let times : [String : Int] = ["Soft": 5, "Medium": 7, "Hard": 12];
     var countdownTimer: Timer = Timer();
     var currentTime = 60;
@@ -48,6 +50,7 @@ class ViewController: UIViewController {
             progressBar.progress = 0.0;
             countdownTimer.invalidate();
             titleLabel.text = "Done!";
+            playSound(soundName: "alarm_sound");
         }
     }
 
@@ -57,6 +60,28 @@ class ViewController: UIViewController {
         let minutes: Int = (totalSeconds / 60) % 60
         //     let hours: Int = totalSeconds / 3600
         return String(format: "%02d:%02d", minutes, seconds)
+    }
+    
+    func playSound(soundName: String!) {
+        guard let url = Bundle.main.url(forResource: soundName, withExtension: "mp3") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
 }
 
